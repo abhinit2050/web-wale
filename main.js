ee2f05838a07a61d4994b148575df172c8b8b83b
Abhinit Rai <abhinitrai@Abhinits-MacBook-Pro.local>
2023-04-06 23:03:40 +0530

    game fully integrated

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 const target = document.getElementById('target');
 const parentHeight = document.getElementById('game').offsetHeight;
 let parentWidth = document.getElementById('game').offsetWidth;
-// let parentMarginTop = Number(window.getComputedStyle(target).getPropertyValue("margin-top").replace("px",''))+450;
-let parentMargin = window.getComputedStyle(target).getPropertyValue("margin-top");
 let targetHeight = document.getElementById('target').offsetHeight;
 let targetWidth = document.getElementById('target').offsetWidth;
+const score = document.getElementById("score");
 
+let count =0;
+let i=0;
     setInterval(()=>{
-        let targetX = Math.random()*(parentWidth);
-        let targetY = Math.random()*((parentHeight)-300);
+        if(i<10){
+            let targetX = Math.random()*(parentWidth);
+        let targetY = Math.random()*(parentHeight-100);
         target.style.position = "absolute";
         target.style.left = targetX+"px";
         target.style.top = targetY+"px";
-    },5000)    
+        i++;
+        } else{
+            target.style.setProperty("display","none");
+            if(count>=3){
+                score.innerHTML = "Congratulations! You are eligible for 10% discount!!"
+            } else{
+                score.innerHTML = "Sorry! Refresh the page to try again!"
+            }
+        }
+        
+    },1500)    
         
                 
 const crosshair = document.getElementById('game');
 const gunshot = document.getElementById('gunshot');
 
+
 crosshair.addEventListener("click", (e)=>{
+
+    console.log(count);
 
     const gunshotLeft = (e.offsetX-5);
     const gunshotTop = (e.offsetY-5);
@@ -27,19 +42,24 @@ crosshair.addEventListener("click", (e)=>{
     gunshot.style.left= gunshotLeft+"px";
     gunshot.style.top = gunshotTop+"px";
     gunshot.style.setProperty('display','block');
-    //crosshair.appendChild(gunshot);
 
     const shotPositionX = (e.x);
     const shotPositionY = (e.y);
     const targetPositionX = (target.getBoundingClientRect().left);
     const targetPositionY = (target.getBoundingClientRect().top);
-    console.log("shotposition X and Y = "+shotPositionX+" "+shotPositionY);
-    console.log("targetposition X and Y = "+targetPositionX+" "+targetPositionY);
+    
+    // console.log("shotposition X and Y = "+shotPositionX+" "+shotPositionY);
+    // console.log("targetposition X and Y = "+targetPositionX+" "+targetPositionY);
+
+    
 
     if((shotPositionX <= targetPositionX+20 && shotPositionY <= targetPositionY+20) && 
     
         (shotPositionX >= targetPositionX-20 && shotPositionY >= targetPositionY-20)) {
         console.log("It's a HIT!");
+        count++;
+        score.innerHTML = count;
+
     } else {
         console.log("It's a Miss!");
     }
